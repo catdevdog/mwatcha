b7c23730de3b5d6e7873a6aebc28b41792343d19
catdevdog <catdevdog@gmail.com>
2023-12-30 02:11:23 +0900

    배포 테스트

--- src/main.js
+++ src/main.js
@@ -21,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 export const db = getFirestore(app);
 
 const vueApp = createApp(App);
