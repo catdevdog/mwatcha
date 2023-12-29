f8c9c463f5bf817c511e779a030a9d0985a4e9d6
catdevdog <catdevdog@gmail.com>
2023-12-29 23:49:30 +0900

    * firebase

--- src/main.js
+++ src/main.js
@@ -4,8 +4,28 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/styles/common.scss";
 
-const app = createApp(App);
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.use(router);
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDBN7-a1wgzqLIVRdbbP_EvQ3IRBsv41o",
+  authDomain: "mwatcha.firebaseapp.com",
+  projectId: "mwatcha",
+  storageBucket: "mwatcha.appspot.com",
+  messagingSenderId: "282714271242",
+  appId: "1:282714271242:web:6d797b35a45bb4b24679f3",
+  measurementId: "G-9ZM6KBTKEH",
+};
 
-app.mount("#app");
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const vueApp = createApp(App);
+vueApp.config.globalProperties.$analytics = analytics;
+vueApp.use(router);
+vueApp.mount("#app");
