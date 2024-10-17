a1b81a74421da74c1e90b8b35c58abf0eaf0ffb0
catdevdog <catdevdog@gmail.com>
2024-10-17 11:18:35 +0900

    fix: meta, config

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,10 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({});
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "뫗챠!", // 여기에 원하는 타이틀을 넣으세요
+    },
+  },
+};
